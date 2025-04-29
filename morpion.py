763e2fff7e274f579baaec483ffe60b953244c82
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-04-29 09:40:01 +0200

    Ajout de l'affichage de la grille

--- morpion.py
+++ morpion.py
@@ -3,3 +3,9 @@ grille = {
     "B":['_','_','_'],
     "C":['_','_','_']
 }
+
+def afficher_grille(grille: dict) -> None:
+    for ligne, colonnes in grille.items():
+        print(f"{colonnes}")
+
+afficher_grille(grille)
