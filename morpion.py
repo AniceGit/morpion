e03ccd4a4323d4a5b93fe50238fabb1bc71587ff
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-04-29 09:51:12 +0200

    Amélioration de l'affichage

--- morpion.py
+++ morpion.py
@@ -9,3 +9,11 @@ def afficher_grille(grille: dict) -> None:
         print(f"{colonnes}")
 
 afficher_grille(grille)
+
+
+def aff_grille(grille: dict) -> None:
+    print(f"   | 1 | 2 | 3 |")
+    for ligne , colonnes in grille.items():
+        print(f" {ligne} | {colonnes[0]} | {colonnes[1]} | {colonnes[2]} |")
+
+aff_grille(grille)
